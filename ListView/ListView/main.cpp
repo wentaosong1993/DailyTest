f6944be7d3d39accf145a5f4538b0b286f2aae88
songwentao <614425032@qq.com>
2019-08-04 15:42:08 +0800

    【QListView】重新定义QAbstractListModel

---
+++ ListView/ListView/main.cpp
@@ -0,0 +1,22 @@
+﻿#include "widget.h"
+#include <QApplication>
+#include "stringlistmodel.h"
+#include <QListView>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+    QStringList numbers;
+    numbers << "One" << "Two" << "Three" << "Four" << "Five";
+
+    QAbstractItemModel *model = new StringListModel(numbers);
+    //要注意的是，这里把StringListModel作为一个QAbstractItemModel来使用。这样我们就可以
+    //使用model中的抽象接口，而且如果将来我们用别的model代替了当前这个model,这些代码也会照样工作。
+    //QListView提供的列表视图足以满足当前这个model的需要了。
+    QListView *view = new QListView;
+    view->setModel(model);
+    view->show();
+
+    return a.exec();
+}
---
+++ ListView/ListView/stringlistmodel.h
@@ -0,0 +1,70 @@
+﻿#ifndef STRINGLISTMODEL_H
+#define STRINGLISTMODEL_H
+
+#include <QAbstractListModel>
+
+class StringListModel : public QAbstractListModel
+{
+    Q_OBJECT
+public:
+    ///
+    /// \构造函数 StringListModel
+    /// \param strings
+    /// \param parent
+    ///
+    StringListModel(const QStringList &strings,QObject *parent = Q_NULLPTR)
+        :QAbstractListModel(parent),stringList(strings){}
+
+    /// \获取行数 rowCount
+    /// \param parent
+    /// \return
+    int rowCount(const QModelIndex &parent = QModelIndex()) const;
+
+    /// 获取数据方法 data
+    /// \param index
+    /// \param role
+    /// \return
+    QVariant data(const QModelIndex &index, int role) const;
+
+
+    /// 获取标题数据方法 headerData
+    /// \param section
+    /// \param orientation
+    /// \param role
+    /// \return
+    QVariant headerData(int section, Qt::Orientation orientation,
+                             int role = Qt::DisplayRole) const;
+
+    /// \数据可编辑标志方法 flags
+    /// \param index
+    /// \return
+    Qt::ItemFlags flags(const QModelIndex &index) const;
+
+    /// \编辑数据的方法 setData
+    /// \param index
+    /// \param value
+    /// \param role
+    /// \return
+    bool setData(const QModelIndex &index, const QVariant &value,
+                     int role = Qt::EditRole);
+
+    /// \插入行方法 insertRows
+    /// \param position
+    /// \param rows
+    /// \param index
+    /// \return
+    bool insertRows(int position, int rows, const QModelIndex &index = QModelIndex());
+
+    /// \删除行方法 removeRows
+    /// \param position
+    /// \param rows
+    /// \param index
+    /// \return
+    bool removeRows(int position, int rows, const QModelIndex &index = QModelIndex());
+
+
+private:
+    QStringList stringList;
+};
+
+#endif // STRINGLISTMODEL_H
