2a61bac8383eb5a21e53e580b908177b1bbc3249
songwentao <614425032@qq.com>
2019-08-15 23:46:42 +0800

    QT实现QMessageBox的按钮中文显示

---
+++ MessageBox/MessageBox/widget.cpp
@@ -0,0 +1,32 @@
+﻿#include "widget.h"
+#include <QMessageBox>
+#include <QDebug>
+#include <QApplication>
+#include <QDesktopWidget>
+
+
+Widget::Widget(QWidget *parent)
+    : QWidget(parent)
+{
+    megBox = new QMessageBox(this);
+    megBox->addButton(QStringLiteral("是"),QMessageBox::AcceptRole);
+    megBox->addButton(QStringLiteral("否"),QMessageBox::RejectRole);
+    megBox->setText(QStringLiteral("退出游戏？"));
+    megBox->move((QApplication::desktop()->screenGeometry().width()/2 - megBox->width()/2),(QApplication::desktop()->screenGeometry().height()/2 - megBox->height()/2));
+    int ret= megBox->exec();
+    switch(ret)
+    {
+    case QMessageBox::AcceptRole:
+    {
+        qDebug() << "close";
+        break;
+    }
+    default:
+        break;
+    }
+}
+
+Widget::~Widget()
+{
+
+}
---
+++ MessageBox/MessageBox/widget.h
@@ -0,0 +1,21 @@
+﻿#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QWidget>
+
+class QMessageBox;
+
+class Widget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    Widget(QWidget *parent = 0);
+    ~Widget();
+
+private:
+    QMessageBox * megBox = nullptr;
+
+};
+
+#endif // WIDGET_H
