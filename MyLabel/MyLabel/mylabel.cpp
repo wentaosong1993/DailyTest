2ec591d8ad41aed92d7cf58e34979d836304a5ba
songwentao <614425032@qq.com>
2019-08-21 20:17:39 +0800

    使用QLabel显示图片，设置大小和标签大小相适应

--- MyLabel/MyLabel/mylabel.cpp
+++ MyLabel/MyLabel/mylabel.cpp
@@ -14,9 +14,15 @@ MyLabel::MyLabel(QWidget *parent)
 //    pushButton->setText("按钮");
 
     label = new QLabel(this);
+    label->setFixedSize(400,400);
 //    label->setText("<a href='www.baidu.com'>百度一下</a>");
 //    connect(label, SIGNAL(linkActivated(QString)), this, SLOT(linkClicked(QString)));
-    label->setPixmap(QPixmap("./first.png"));
+//    label->setPixmap(QPixmap("./first.png"));
+
+    QPixmap pix("../first.png");
+    QPixmap pix2 = pix.scaled(label->width(), label->height());//设置大小和标签大小相适应
+    label->setPixmap(pix2);
+
 
     //D:/GitHub/DailyTest/MyLabel/MyLabel/Debug/debug --- Qt
     //D:\GitHub\DailyTest\MyLabel\MyLabel\Debug\debug --- Windows
