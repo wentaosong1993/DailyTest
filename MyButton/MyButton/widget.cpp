acaed8ad30ed9e0c1c6063b7137edc8a6ea1c11f
songwentao <614425032@qq.com>
2019-08-21 23:17:51 +0800

    给按钮添加快捷键
     方法1：pushButton->setText("Down&load");//&在谁前面，Alt+谁 就是对应的快捷键
     方法二： pushButton->setShortcut(QKeySequence("Ctrl+P"));

---
+++ MyButton/MyButton/widget.cpp
@@ -0,0 +1,27 @@
+﻿#include "widget.h"
+#include <QPushButton>
+#include <QDebug>
+#include <QKeySequence>
+
+Widget::Widget(QWidget *parent)
+    : QWidget(parent)
+{
+    pushButton = new QPushButton(this);
+    //方法一：
+//    pushButton->setText("&Download");
+//    pushButton->setText("Down&load");//&在谁前面，Alt+谁 就是对应的快捷键
+
+    //方法二：
+    pushButton->setShortcut(QKeySequence("Ctrl+P"));
+    connect(pushButton,SIGNAL(clicked(bool)),this,SLOT(myButtonClick(bool)));
+}
+
+Widget::~Widget()
+{
+
+}
+
+void Widget::myButtonClick(bool isCheck)
+{
+    qDebug() << QStringLiteral("状态：") << isCheck;
+}
---
+++ MyButton/MyButton/widget.h
@@ -0,0 +1,22 @@
+﻿#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QWidget>
+class QPushButton;
+
+class Widget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    Widget(QWidget *parent = 0);
+    ~Widget();
+
+public slots:
+    void myButtonClick(bool isCheck);
+
+private:
+    QPushButton *pushButton;
+};
+
+#endif // WIDGET_H
