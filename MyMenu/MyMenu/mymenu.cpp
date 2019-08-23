bc4fd9434f2db519097e3ca28ae9ec5e2272c598
songwentao <614425032@qq.com>
2019-08-24 00:02:23 +0800

    菜单按钮

---
+++ MyMenu/MyMenu/mymenu.cpp
@@ -0,0 +1,51 @@
+﻿#include "mymenu.h"
+#include <QMenu>
+#include <QAction>
+
+MyMenu::MyMenu(QWidget *parent)
+    : QWidget(parent)
+{
+    menuButton = new QPushButton(QStringLiteral("菜单按钮"),this);
+    menuButton->setFixedSize(100,30);
+    menuButton->setStyleSheet("QPushButton:menu-indicator{image:None;}");//去掉按钮后面那个小箭头
+
+    menu = new QMenu(menuButton);
+
+    action1 = new QAction(QStringLiteral("菜单一"),menu);
+    action2 = new QAction(QStringLiteral("菜单二"),menu);
+
+///error:
+/*  const *T
+    QList<QAction> listActions;
+    listActions.append(action1);
+    listActions.append(action2);
+    listActions[0] = action1;
+    listActions[1] = action2;
+    menu->addActions(listActions);
+*/
+    menu->addAction(action1);
+    menu->addAction(action2);
+
+    menuButton->setMenu(menu);
+
+    connect(action1, SIGNAL(triggered(bool)), this, SLOT(textChanged1(bool)));
+    connect(action2, SIGNAL(triggered(bool)), this, SLOT(textChanged2(bool)));
+
+
+}
+
+MyMenu::~MyMenu()
+{
+    delete menuButton;
+    menuButton = nullptr;
+}
+
+void MyMenu::textChanged1(bool text)
+{
+    menuButton->setText(QStringLiteral("菜单一被触发"));
+}
+
+void MyMenu::textChanged2(bool text)
+{
+    menuButton->setText(QStringLiteral("菜单二被触发"));
+}
---
+++ MyMenu/MyMenu/mymenu.h
@@ -0,0 +1,26 @@
+﻿#ifndef MYMENU_H
+#define MYMENU_H
+
+#include <QWidget>
+#include <QPushButton>
+
+class MyMenu : public QWidget
+{
+    Q_OBJECT
+
+public:
+    MyMenu(QWidget *parent = 0);
+    ~MyMenu();
+
+public slots:
+    void textChanged1(bool text);
+    void textChanged2(bool text);
+
+private:
+    QPushButton *menuButton = nullptr;
+    QMenu *menu = nullptr;
+    QAction *action1 = nullptr;
+    QAction *action2 = nullptr;
+};
+
+#endif // MYMENU_H
