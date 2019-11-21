ed44ad6fcd0b7c476e146f64ee6069ec592535df
songwentao <614425032@qq.com>
2019-11-21 20:43:57 +0800

    1、应用qMax过程中出现的问题，测试demo
    1、获取最大值过程中，不要给自己赋值，这样会出现覆盖。

---
+++ Reference/Reference/main.cpp
@@ -0,0 +1,22 @@
+﻿#include <QCoreApplication>
+#include <QtGlobal>
+#include <QDebug>
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+
+    int numberArray[] = {1,8,2,3,6};
+
+    int maxNumber = numberArray[0];
+    for(int i = 0; i < sizeof(numberArray)/sizeof(numberArray[0]); i++)
+    {
+//        maxNumber = numberArray[i];//每次都会重写该值
+        maxNumber = qMax(maxNumber,numberArray[i]);
+    }
+
+    qInfo() << "maxNumber:" << maxNumber;
+
+    return a.exec();
+}
